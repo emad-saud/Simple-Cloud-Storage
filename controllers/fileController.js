6d91b2e9c01496b816b7202b40fabcb11387aa09
Emad Saud <emad.k.saud@gmail.com>
2025-02-26 19:05:57 +0200

    Added Dockerfile and fixed some minor problems

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,71 +1,67 @@
-const mongoose = require('mongoose');
-const multer = require('multer');
-const path = require('path');
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 
-const File = require('../models/fileModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const File = require("../models/fileModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'storage');
-  },
-  filename: (req, file, cb) => {
-    const firstOctet = file.mimetype.split('/')[0];
-    let ext = file.mimetype.split('/')[1];
-    if (firstOctet === 'text') ext = 'txt';
-    cb(null, `user-${req.user._id}-${Date.now()}-${file.originalname}`);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "storage");
+	},
+	filename: (req, file, cb) => {
+		const firstOctet = file.mimetype.split("/")[0];
+		let ext = file.mimetype.split("/")[1];
+		if (firstOctet === "text") ext = "txt";
+		cb(null, `user-${req.user._id}-${Date.now()}`);
+	},
 });
 
 const upload = multer({
-  storage: multerStorage,
+	storage: multerStorage,
 });
 
-exports.uploadFile = upload.single('file');
+exports.uploadFile = upload.single("file");
 
 exports.storeFile = catchAsync(async (req, res, next) => {
-  const file = req.file || null;
-  if (!file) {
-    return next(
-      new AppError('no file is found in request please try again later!', 400)
-    );
-  }
+	const file = req.file || null;
+	if (!file) {
+		return next(
+			new AppError("no file is found in request please try again later!", 400)
+		);
+	}
 
-  console.log(file);
+	console.log(file);
 
-  const doc = await File.create({
-    name: req.file.filename,
-    originalName: req.file.originalname,
-    user: req.user._id,
-  });
+	const doc = await File.create({
+		name: req.file.filename,
+		originalName: req.file.originalname,
+		user: req.user._id,
+	});
 
-  res.status(200).json({
-    status: 'success',
-    message: 'file uploaded successfully',
-  });
+	res.status(200).json({
+		status: "success",
+		message: "file uploaded successfully",
+	});
 });
 
 exports.getFile = catchAsync(async (req, res, next) => {
-  const file = await File.findOne({
-    _id: req.params.fileId,
-    user: req.user._id,
-  });
+	const file = await File.findOne({
+		_id: req.params.fileId,
+		user: req.user._id,
+	});
 
-  // console.log(req.params.fileId);
-  // console.log(req.user._id);
+	if (!file) {
+		return next(new AppError("No File found with that id!", 404));
+	}
 
-  if (!file) {
-    return next(new AppError('No File found with that id!', 404));
-  }
+	const filePath = path.join(__dirname, `../storage/${file.originalName}`);
 
-  const filePath = path.join(__dirname, `../storage/${file.name}`);
-
-  res.setHeader('Content-Type', 'application/octet-stream');
-  res.setHeader('Content-Disposition', `attachment; filename="${file.name}"`);
-  res.download(filePath, function (err) {
-    if (err) {
-      next();
-    }
-  });
+	res.setHeader("Content-Type", "application/octet-stream");
+	res.setHeader(
+		"Content-Disposition",
+		`attachment; filename="${file.originalName}"`
+	);
+	res.download(filePath, file.originalName);
 });
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/config.env` });
 
-const app = require('./app');
+const app = require("./app");
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL)
-  .then(() => {
-    console.log('DB connection successful!');
-  })
-  .catch((err) => {
-    console.log('failed to connect to the database! exiting the program...');
-    process.exit(1);
-  });
+	.connect(process.env.DATABASE_URL)
+	.then(() => {
+		console.log("DB connection successful!");
+	})
+	.catch((err) => {
+		console.log("failed to connect to the database! exiting the program...");
+		process.exit(1);
+	});
 
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-  console.log(`App is running on port ${port}...`);
+	console.log(`App is running on port ${port}...`);
 });
 
-process.on('unhandledRejection', (err, pr) => {
-  console.log('UNHANDLED REJECTION 💥 Shutting down...');
-  console.log(err.name, err.message);
-  console.log(err.stack);
-  server.close(() => {
-    process.exit(1);
-  });
+process.on("unhandledRejection", (err, pr) => {
+	console.log("UNHANDLED REJECTION 💥 Shutting down...");
+	console.log(err.name, err.message);
+	console.log(err.stack);
+	server.close(() => {
+		process.exit(1);
+	});
 });
